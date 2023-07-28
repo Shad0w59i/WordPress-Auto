96c38030ace3f693f74e725f0f91a5afbca5972c
Shad0w59i <93844477+Shad0w59i@users.noreply.github.com>
2023-07-28 12:53:43 +0200

    🧠 Update install_dependencies.sh

--- functions/install_dependencies.sh
+++ functions/install_dependencies.sh
@@ -1,10 +1,24 @@
+#install first dependencies
+printf -- "\e[0m\e[37m-> Installation des dépendances\e[0m";
+printf -- "\n";
+install_dependencies () {
+	sudo apt-get -y install software-properties-common ca-certificates lsb-release apt-transport-https 1>>$logfile 2>>$errlog
+	printf -- "\e[90mLes dépendances suivantes ont été installées: software-properties-common ca-certificates lsb-release apt-transport-https\e[22m"
+}
+install_dependencies
+echo ""
+echo ""
+
 #get repository
 printf -- "\e[0m\e[37m-> Téléchargement des repository\e[0m";
 printf -- "\n";
 get_ppa () {
 	yes | sudo add-apt-repository ppa:ondrej/php 1>>$logfile 2>>$errlog
+	yes | sudo add-apt-repository ppa:ondrej/nginx-mainline 1>>$logfile 2>>$errlog
 	yes | sudo add-apt-repository ppa:certbot/certbot 1>>$logfile 2>>$errlog
 	printf -- "\e[90mRepository: ppa:ondrej/php\e[22m"
+	printf -- "\n";
+	printf -- "\e[90mRepository: ppa:ondrej/nginx-mainline\e[22m"
 	printf -- "\n";
 	printf -- "\e[90mRepository: ppa:certbot/certbot\e[22m"
 }
@@ -27,8 +41,8 @@ echo ""
 printf -- "\e[0m\e[37m-> Installation des dépendances\e[0m";
 printf -- "\n";
 install_dependencies () {
-	sudo apt-get -y install git nano ntp ntpdate nginx php7.2-fpm php7.2-common php7.2-mbstring php7.2-xmlrpc php7.2-gd php7.2-xml php7.2-mysql php7.2-cli php7.2-zip php7.2-curl php7.2-soap php7.2-intl php7.2-ldap mariadb-server mariadb-client certbot python-certbot-nginx 1>>$logfile 2>>$errlog
-	printf -- "\e[90mLes dépendances suivantes ont été installées: git nano ntp ntpdate nginx php7.2-fpm php7.2-common php7.2-mbstring php7.2-xmlrpc php7.2-gd php7.2-xml php7.2-mysql php7.2-cli php7.2-zip php7.2-curl php7.2-soap php7.2-intl php7.2-ldap mariadb-server mariadb-client certbot python-certbot-nginx\e[22m"
+	sudo apt-get -y install git nano ntp ntpdate nginx php8.2-fpm php8.2-common php8.2-mbstring php8.2-xmlrpc php8.2-gd php8.2-xml php8.2-mysql php8.2-cli php8.2-zip php8.2-curl php8.2-soap php8.2-intl php8.2-ldap mariadb-server mariadb-client certbot python-certbot-nginx 1>>$logfile 2>>$errlog
+	printf -- "\e[90mLes dépendances suivantes ont été installées: git nano ntp ntpdate nginx php8.2-fpm php8.2-common php8.2-mbstring php8.2-xmlrpc php8.2-gd php8.2-xml php8.2-mysql php8.2-cli php8.2-zip php8.2-curl php8.2-soap php8.2-intl php8.2-ldap mariadb-server mariadb-client certbot python-certbot-nginx\e[22m"
 }
 install_dependencies
 echo ""
