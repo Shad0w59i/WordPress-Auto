79dec9996201088febccf0c18c9c4875dca03ac1
Shad0w59i <93844477+Shad0w59i@users.noreply.github.com>
2023-07-28 18:23:21 +0200

    🧠 Update configure_mysql.sh

--- functions/configure_mysql.sh
+++ functions/configure_mysql.sh
@@ -6,7 +6,7 @@ configure_mysql () {
 	# sudo mysql -e "UPDATE mysql.user SET Password = PASSWORD('${dbpass}') WHERE User = 'root'" 1>>$logfile 2>>$errlog &&
 	# sudo mysql -e "FLUSH PRIVILEGES" 1>>$logfile 2>>$errlog
 	sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${dbpass}'" 1>>$logfile 2>>$errlog &&
-	sudo mysql -uroot -p$dbpass -e "FLUSH PRIVILEGES" 1>>$logfile 2>>$errlog
+	mysql -uroot -p$dbpass -e "FLUSH PRIVILEGES" 1>>$logfile 2>>$errlog
 	printf -- "\e[90mMot de passe MySQL mis à jour pour l'utilisateur: root\e[22m"
 	printf -- "\n";
 	printf -- "\e[90mPrivilèges flushed\e[22m"
