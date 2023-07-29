b4be71f90a734efc81c43ba91d8f7c650ffe0a23
Shad0w59i <93844477+Shad0w59i@users.noreply.github.com>
2023-07-30 01:28:23 +0200

    🔖 Update install_php.sh

--- functions/dependencies/install_php.sh
+++ functions/dependencies/install_php.sh
@@ -30,13 +30,13 @@ printf -- "\e[0m\e[37m-> Configuration de PHP\e[0m";
 printf -- "\n";
 configure_php () {
 	sudo sed -i "s/^;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php/8.2/fpm/php.ini 1>>$logfile 2>>$errlog &&
-	sudo sed -i "s/^max_execution_time = 30/max_execution_time = 360/" /etc/php/8.2/fpm/php.ini 1>>$logfile 2>>$errlog &&
+	sudo sed -i "s/^max_execution_time = 30/max_execution_time = 600/" /etc/php/8.2/fpm/php.ini 1>>$logfile 2>>$errlog &&
 	sudo sed -i "s/^memory_limit = 128M/memory_limit = 256M/" /etc/php/8.2/fpm/php.ini 1>>$logfile 2>>$errlog &&
 	sudo sed -i "s/^upload_max_filesize = 2M/upload_max_filesize = 64M/" /etc/php/8.2/fpm/php.ini 1>>$logfile 2>>$errlog &&
 	sudo sed -i "s/^post_max_size = 8M/post_max_size = 64M/" /etc/php/8.2/fpm/php.ini 1>>$logfile 2>>$errlog
 	printf -- "\e[90mcgi.fix_pathinfo fixé à 0 \e[22m"
 	printf -- "\n";
-	printf -- "\e[90mmax_execution_time fixé à 360 \e[22m"
+	printf -- "\e[90mmax_execution_time fixé à 600 \e[22m"
 	printf -- "\n";
 	printf -- "\e[90mlimite_mémoire fixée à 256M \e[22m"
 	printf -- "\n";
